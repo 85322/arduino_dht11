e7c640b2a5b9f4d159c97096fa7cf822540690ae
Anon853 <4m4zon3@web.de>
2020-07-21 08:12:19 +0200

    json file & LED code

--- arduino_dhttest/arduino_dhttest.ino
+++ arduino_dhttest/arduino_dhttest.ino
@@ -4,14 +4,14 @@
 //Constants
 #define DHT11_PIN 2   
 #define DHTTYPE DHT11
-
+#define LED 8
 #define RELAIS_PIN 3
 
 DHT dht(DHT11_PIN, DHTTYPE);
 //Variables
 float peter;  //Stores humidity value
 float temp; //Stores temperature value
-float relaisSwitchOnThreshhold=13.00;
+float relaisSwitchOnThreshhold=20.00;
 
 void setup()
 {
@@ -19,6 +19,7 @@ void setup()
     dht.begin();
     pinMode(RELAIS_PIN, OUTPUT);
     digitalWrite(RELAIS_PIN, HIGH);
+    pinMode(LED, OUTPUT);
 }
 
 void loop()
@@ -34,6 +35,7 @@ void loop()
     Serial.println(" Celsius"); 
     switchRelaisBasedOnSensorHumidity(peter);  
     delay(2000); //Delay 2 sec.
+    digitalWrite(LED, HIGH); 
 }
 
 void switchRelaisBasedOnSensorHumidity(float hum){
