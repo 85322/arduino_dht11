cadae224bb8edff6d3517047aeebd0ce76dc16a8
Anon853 <4m4zon3@web.de>
2020-07-25 13:35:20 +0200

    final version

--- arduino_dhttest/arduino_dhttest.ino
+++ arduino_dhttest/arduino_dhttest.ino
@@ -1,44 +1,84 @@
-//Libraries
+ //Used LCD Pins
+ //2, 3, 4, 5, 6, 11, 12, 3xGND, 2x5V  
+ //
+ //Open pins
+ //9, 10
+ //
+ //DHT11 Pins
+ //8, 1xGND, 1x5V
+ //
+ //RELAY Pins
+ //7, 1x GND, 1x5V
+ //
+ //LED Pins
+ //13, 1xGND
+
+#include <LiquidCrystal.h> 
 #include <DHT.h>
 
-//Constants
-#define DHT11_PIN 2   
-#define DHTTYPE DHT11
-#define LED 8
-#define RELAIS_PIN 3
+#define DHT11_PIN 8   
+#define DHTTYPE DHT12
+#define RELAIS_PIN 7
+#define LED 13
+
 
 DHT dht(DHT11_PIN, DHTTYPE);
-//Variables
-float peter;  //Stores humidity value
-float temp; //Stores temperature value
+int Contrast=100;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+int led = 13;
+
+float hum;  
+float temp; 
 float relaisSwitchOnThreshhold=20.00;
 
-void setup()
-{
+ void setup()
+ {
+    //LCD
+    analogWrite(6,Contrast);
+     lcd.begin(16, 2);
+
+    //DHT11
     Serial.begin(9600);
     dht.begin();
+
+    //RELAY
     pinMode(RELAIS_PIN, OUTPUT);
     digitalWrite(RELAIS_PIN, HIGH);
-    pinMode(LED, OUTPUT);
-}
 
-void loop()
-{
-    //Read data and store it to variables hum and temp
-    peter = dht.readHumidity();
+    //LED
+    pinMode(led, OUTPUT);
+  } 
+
+
+
+
+  
+     void loop()
+ { 
+     lcd.setCursor(0, 0);
+     lcd.print("Hum:  " + String(hum) + "%");
+     lcd.setCursor(0, 1);
+     lcd.print("Temp: " + String(temp) + (char)223 + "C");
+   
+    hum = dht.readHumidity();
     temp= dht.readTemperature();
-    //Print temp and humidity values to serial monitor
+    
     Serial.print("Humidity: ");
-    Serial.print(peter);
+    Serial.print(hum);
     Serial.print(" %, Temp: ");
     Serial.print(temp);
     Serial.println(" Celsius"); 
-    switchRelaisBasedOnSensorHumidity(peter);  
-    delay(2000); //Delay 2 sec.
-    digitalWrite(LED, HIGH); 
-}
+    delay(2000);
+    switchRelaisBasedOnSensorHumidity(hum);
+    
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+ }
 
-void switchRelaisBasedOnSensorHumidity(float hum){
+
+ void switchRelaisBasedOnSensorHumidity(float hum){
        if (isHumidityAboveThreshhold(hum)){
         digitalWrite(RELAIS_PIN, LOW);
         Serial.println(" Switching Relay");       
@@ -54,6 +94,3 @@ bool isHumidityAboveThreshhold(float hum){
     }
     return false;
 }
-
-
-
